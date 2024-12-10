41152eace240ebb89b86728914ca811501743c0d
heondolee <heondo999@inu.ac.kr>
2024-12-10 10:00:49 +0900

    SwiftUI 토글 테스트

--- Reforest/Social/SocialView.swift
+++ Reforest/Social/SocialView.swift
@@ -1,19 +1,36 @@
-//
-//  SocialView.swift
-//  Reforest
-//
-//  Created by Heondo Lee on 12/3/24.
-//
-
-import Foundation
 import SwiftUI
 
 struct SocialView: View {
+    @State private var items: [ListItem] = [
+        ListItem(title: "1. 신중하고 공감을 잘함", children: [
+            ListItem(title: "신중하고 공감을 잘함", children: [
+                ListItem(title: "신중하고 공감을 잘함"),
+            ]),
+            ListItem(title: "신중하고 공감을 잘함")
+        ]),
+        ListItem(title: "2. 신중하고 공감을 잘함", children: [
+            ListItem(title: "신중하고 공감을 잘함"),
+        ])
+    ]
+
     var body: some View {
-        Text("소셜하면 입니다.")
+        List(items, children: \.children) { item in
+            HStack {
+                Text(item.title)
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
     }
 }
 
-#Preview {
-    SocialView()
+struct ListItem: Identifiable {
+    let id = UUID()
+    let title: String
+    var children: [ListItem]? = nil
+}
+
+struct SocialView_Previews: PreviewProvider {
+    static var previews: some View {
+        SocialView()
+    }
 }
